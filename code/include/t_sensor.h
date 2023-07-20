1d93cb5005713fd6f5e8cc172fb51853c817b38e
DrNotThatEvil <wilvin@wilv.in>
2023-07-20 17:43:55 +0200

    WIP: Adding files

--- code/include/t_sensor.h
+++ code/include/t_sensor.h
@@ -17,39 +17,58 @@
 //  Abnormal deviations
 //  A to big delta between sensor 1 & 2
 
-#define T_VCC 5
-#define T_SENSOR_R 100000 // 100K
-#define T_SERIES_R 100000 // 100K
+#define T_SENSOR0_NOMR 100000       // 100K resistance at 25 c
+#define T_SENSOR0_NOMT 298.15f       // temp for norminal resistance in kelvin
+#define T_SENSOR0_SERIESR 100000    // 100K series resistor
+#define T_SENSOR0_BCOEFF 3950       // temp for norminal resistance
+
+#define T_SENSOR1_NOMR 100000       // 100K resistance at 25 c
+#define T_SENSOR1_NOMT 298.15f      // temp for norminal resistance in kelvin
+#define T_SENSOR1_SERIESR 100000    // 100K series resistor
+#define T_SENSOR1_BCOEFF 3950       // temp for norminal resistance
 
 #define T_SENSOR_MAX_OUTLIER_COUNT 5
-#define T_SENSOR_OUTLIER_COUNT_RST 10
+#define T_SENSOR_OUTLIER_RST_COUNT 5
+
+#define T_SENSOR_BAD_MIN 10
+#define T_SENSOR_BAD_MAX 200
+#define T_SENSOR_BAD_MAX_DIFF 5
 
 // 0 -- 1 -- 2 -- 3 -- 4 -- 5 -- 6 -- 7 --  8 -- 9 
 
 uint8_t t_sensor_initialized = 0;
+struct t_sensor_state;
+
+typedef void (*t_sensor_error_cb)(struct t_sensor_state* state_ptr);
+
+enum t_sanity_state {
+    NORMAL,
+    HAS_OUTLIERS,
+    ERRORED
+};
 
 typedef struct {
-    uint16_t* sensor_one_readings;
-    float sensor_one_abc[3];
-    //uint16_t* temp_sensor_one_deviations;
+    uint16_t* s0_readings;
+    uint16_t* s1_readings;
 
-    uint16_t* sensor_two_readings;
-    float sensor_two_abc[3];
-    //uint16_t* temp_sensor_two_deviations;
-
-    uint8_t sensor_one_outlier_cnt;
-    uint8_t sensor_two_outlier_cnt;
+    uint8_t s_outlier_cnt;
+    uint8_t s_outlier_rst_cnt;
 
     uint8_t cur_reading_index;
     uint8_t max_reading_history;
+
+    t_sensor_sanity_state sanity_state;
+    t_sensor_error_cb error_cb;
 } t_sensor_state;
 
 void t_sensor_init(
     t_sensor_state* state_ptr,
-    float a1, float b1, float c1,
-    float a2, float b2, float c2,
-    uint8_t max_history
+    uint8_t max_history,
+    t_sensor_error_cb error_cb
 );
+
+int t_sensor_sanitycheck(t_sensor_state* state_ptr, float s0_temp, float s1_temp);
+float t_sensor_read(t_sensor_state* state_ptr, int sensor);
 
 void t_sensor_tick(t_sensor_state* state_ptr);
 
--- code/src/t_sensor.c
+++ code/src/t_sensor.c
@@ -2,22 +2,104 @@
 #include <stdlib.h>
 
 void t_sensor_init(
-    t_sensor_state* state_ptr, 
-    float a1, float b1, float c1,
-    float a2, float b2, float c2,
-    uint8_t max_history
+    t_sensor_state* state_ptr,
+    uint8_t max_history,
+    t_sensor_error_cb error_cb
 )
 {
     state_ptr->cur_reading_index = 0;
-    state_ptr->sensor_one_outlier_cnt = 0;
-    state_ptr->sensor_two_outlier_cnt = 0;
+    state_ptr->s_outlier_cnt = 0;
+    state_ptr->s_outlier_rst_cnt = 0;
 
     state_ptr->max_reading_history = max_history;
-    state_ptr->sensor_one_readings = malloc(sizeof(uint16_t) * max_history);
-    state_ptr->sensor_two_readings = malloc(sizeof(uint16_t) * max_history);
+    state_ptr->s0_readings = malloc(sizeof(uint16_t) * max_history);
+    state_ptr->s1_readings = malloc(sizeof(uint16_t) * max_history);
+
+    state_ptr->sanity_state = NORMAL;
+    state_ptr->error_cb = error_cb;
+
+    for(u_int8_t i = 0; i < max_history; i++)
+    {
+        state_ptr->s0_readings[i] = 0;
+        state_ptr->s1_readings[i] = 0;
+    }
 }
 
-void t_sensor_tick(t_sensor_state* state_ptr)
+int t_sensor_sanitycheck(t_sensor_state *state_ptr, float s0_temp, float s1_temp)
 {
+    int sensor_diff = abs(s0_temp - s1_temp);
+
+    if (s0_temp < T_SENSOR_BAD_MIN || s0_temp > T_SENSOR_BAD_MAX || s1_temp < T_SENSOR_BAD_MIN || s1_temp > T_SENSOR_BAD_MAX)
+    {
+        state_ptr->s_outlier_cnt++;
+        state_ptr->sanity_state = HAS_OUTLIERS;
+        state_ptr->s_outlier_rst_cnt = 0; // bad readings reset the good recovery counter
+
+        if(state_ptr->s_outlier_cnt > T_SENSOR_MAX_OUTLIER_COUNT) {
+            // Call safety routine.
+            state_ptr->sanity_state = ERRORED;
+            state_ptr->error_cb(state_ptr);
+        }
+
+        return 1;
+    }
+
+    if(sensor_diff > T_SENSOR_BAD_MAX_DIFF)
+    {
+        state_ptr->s_outlier_cnt++;
+        state_ptr->sanity_state = HAS_OUTLIERS;
+        state_ptr->s_outlier_rst_cnt = 0; // bad readings reset the good recovery counter
+
+        if(state_ptr->s_outlier_cnt > T_SENSOR_MAX_OUTLIER_COUNT) {
+            // Call safety routine.
+            state_ptr->sanity_state = ERRORED;
+            state_ptr->error_cb(state_ptr);
+        }
+
+        return 1;
+    }
+
+    if (state_ptr->s_outlier_cnt > 0)
+    {
+        // Good reading but still recovering from bad ones.
+        state_ptr->s_outlier_rst_cnt++;
+
+        if (state_ptr->s_outlier_rst_cnt > T_SENSOR_OUTLIER_RST_COUNT) {
+            // reset counters.
+            state_ptr->sanity_state = NORMAL;
+            state_ptr->s_outlier_cnt = 0;
+            state_ptr->s_outlier_rst_cnt = 0;
+        }
+
+        return 1;
+    }
+
+    return 0;
+}
+
+float t_sensor_read_raw(t_sensor_state* state_ptr, int sensor)
+{
+    adc_select_input(sensor);
+
+    float s_read = adc_read();
+    s_read = T_SENSOR0_SERIESR / ((4095 / s_read) - 1);
+
+    return (1.0f / ((log(s_read / T_SENSOR0_NOMR) / T_SENSOR0_BCOEFF) + 1.0f / T_SENSOR0_NOMT)) - 273.15f;
+}
+
+void t_sensor_tick(t_sensor_state *state_ptr)
+{
+    float s0 = t_sensor_read_raw(state_ptr, 0);
+    float s1 = t_sensor_read_raw(state_ptr, 1);
+
+    int sanity_check = t_sensor_sanitycheck(state_ptr, s0, s1);
+    if (sanity_check > 0)
+    {
+        // Sanity check failed
+        // Check if errored that kind of stuff.
+        return;
+    }
+
     
+
 }
